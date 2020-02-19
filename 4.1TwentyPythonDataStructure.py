84532e6146a422c1daff062e5bd5d5007c9a1677
Noah Melngailis <noah.melngailis@gmail.com>
2020-02-19 16:03:16 -0600

    python practice back up

---
+++ 4.1TwentyPythonDataStructure.py
@@ -0,0 +1,24 @@
+# How many students are there?
+len(students)
+# How many students prefer light coffee? For each type of coffee roast?
+len([student for student in students if student['coffee_preference'] == 'lig
+     ...: ht']) 
+# How many types of each pet are there?
+
+# How many grades does each student have? Do they all have the same number of grades?
+# What is each student's grade average?
+# How many pets does each student have?
+# How many students are in web development? data science?
+# What is the average number of pets for students in web development?
+# What is the average pet age for students in data science?
+# What is most frequent coffee preference for data science students?
+# What is the least frequent coffee preference for web development students?
+# What is the average grade for students with at least 2 pets?
+# How many students have 3 pets?
+# What is the average grade for students with 0 pets?
+# What is the average grade for web development students? data science students?
+# What is the average grade range (i.e. highest grade - lowest grade) for dark coffee drinkers?
+# What is the average number of pets for medium coffee drinkers?
+# What is the most common type of pet for web development students?
+# What is the average name length?
+# What is the highest pet age for light coffee drinkers?
--- 4.1list_comprehension_practice.py
+++ 4.1list_comprehension_practice.py
@@ -1,16 +1,28 @@
 uppercased_fruits = [fruit.upper() for fruit in fruits]    
-capitalized_fruits = [fruit[0].upper() +  fruit[1:] for fruit in fruits]   
+capitalized_fruits = [fruit[0].upper() +  fruit[1:] for fruit in fruits]
+capitalized_fruits2 = [fruit.capitalize() for fruit in fruits]   
+
 # Exercise 3 - Use a list comprehension to make a variable named 
 # fruits_with_more_than_two_vowels. Hint: You'll need a way to 
 # check if something is a vowel.
-def is_vowel(word):
-    if word == 'a' or 'e' or 'i' or 'o' or 'u':
-        True
-    else:
-        False
+fruits_with_more_than_two_vowels = [
+    fruit for fruit in fruits if fruit.count('a') + fruit.count('e') + 
+    fruit.count('i') + fruit.count('o') + fruit.count('u') > 2
+]
+#or
+def is_vowel(string):
+    return string.lower() in "aeiou" and len(string) == 1
 
-is_vowel(a)
+def count_vowels(string):
+    string = string.lower()
+    return string.count('a') + string.count('e') + string.count('i') + string.count('o') + string.count('u')
 
+[fruit for fruit in fruits if count_vowels(fruit) > 2]
+
+#or for loop
+
+# Exercise 4 - make a variable named fruits_with_only_two_vowels. The result should be ['mango', 'kiwi', 'strawberry']
+[fruit for fruit in fruits if count_vowels(fruit) == 2]
 # Exercise 5 - make a list that contains each fruit with more than 5 characters
 [fruit for fruit in fruits if len(fruit) > 5 ]
 # Exercise 6 - make a list that contains each fruit with exactly 5 characters
@@ -20,7 +32,8 @@ is_vowel(a)
 # Exercise 8 - Make a list containing the number of characters in each fruit. Output would be [5, 4, 10, etc... ]
 [len(fruit) for fruit in fruits]
 # Exercise 9 - Make a variable named fruits_with_letter_a that contains a list of only the fruits that contain the letter "a"
-'fruits_with_letter_a = [fruit for fruit in fruits if fruit == ]'
+fruits_with_letter_a = [fruit for fruit in fruits if fruit.count('a') > 0 ]'
+fruits_with_letter_a = [fruit for fruit in fruits if 'a' in fruit]
 # Exercise 10 - Make a variable named even_numbers that holds only the even numbers 
 even_numbers = [number for number in numbers if number % 2 == 0]  
 # Exercise 11 - Make a variable named odd_numbers that holds only the odd numbers
@@ -31,6 +44,7 @@ positive_numbers = [number for number in numbers if number > 0]
 negative_numbers = [number for number in numbers if number < 0]
 # Exercise 14 - use a list comprehension w/ a conditional in order to produce a list of numbers with 2 or more numerals
 [number for number in numbers if len(str(number)) > 1 and number > 0 or len(str(number)) > 2 and number < 0]
+
 # Exercise 15 - Make a variable named numbers_squared that contains the numbers list with each element squared. Output is [4, 9, 16, etc...]
 numbers_squared = [number**2 for number in numbers]
 # Exercise 16 - Make a variable named odd_negative_numbers that contains only the numbers that are both odd and negative.
