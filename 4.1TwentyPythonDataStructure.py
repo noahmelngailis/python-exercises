90e1dc2eae4ce2b7dc986c367ef135008cfe1476
Noah Melngailis <noah.melngailis@gmail.com>
2020-02-20 20:37:07 -0600

    4.1 Data Structure Problems

--- 4.1TwentyPythonDataStructure.py
+++ 4.1TwentyPythonDataStructure.py
@@ -19,16 +19,24 @@ set(grades)
 # What is each student's grade average?
 grades2 = [student['grades']) for student in students]
 avg_grade = 
-[student['student'] + str(sum(student['grades'])/len(student['grades'])) for student in students]
+[student['student'] + "  :  " + str(sum(student['grades'])/len(student['grades'])) for student in students]
 
 # How many pets does each student have?
-[student['student'] + str(len(student['pets'])) for student in students]
-[student['student'] for student in students]
+[student['student'] + "  :  " + str(len(student['pets'])) for student in students]
+
 # How many students are in web development? data science?
-[set(student['course']) + len(student['course']) for student in students]
+num_data_science = len([student['course'] for student in students if student['course'] == 'data science'])
+num_web_dev = len([student['course'] for student in students if student['course'] == 'web development'])
 # What is the average number of pets for students in web development?
+flatten = sum([student['pets'] for student in students if student['course'] == 'web development'],[])
+avg_pet_per_student = len(flatten)/len([student for student in students if student['course'] == 'web development'])
 # What is the average pet age for students in data science?
+data_sci = [student['pets'] for student in students if student['course'] == 'data science']
+flatten_data_sci = sum(data_sci, [])
+f_d_s = [flat['age'] for flat in flatten_data_sci]
+sum(f_d_s)/len(f_d_s)
 # What is most frequent coffee preference for data science students?
+cof_dat = [student['coffee_preference'] for student in students if student['course'] == 'data science']
 # What is the least frequent coffee preference for web development students?
 # What is the average grade for students with at least 2 pets?
 # How many students have 3 pets?
