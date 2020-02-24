ab3480305d22f71030a60c10776d79526cdc2b58
Noah Melngailis <noah.melngailis@gmail.com>
2020-02-24 13:18:19 -0600

    Functions with instructor

--- 4.5_function_exercises.py
+++ 4.5_function_exercises.py
@@ -4,13 +4,25 @@ def is_two(x):
         return True
     else:
         return False
+def is_two(x):
+    return x in [2, '2']
+    
 
 #2
 def is_vowel(x):
-    if x in ['a', 'e', 'i', 'o', 'u']:
-        return True
-    else:
-        return False
+    x = x.lower()
+    return x in ['a', 'e', 'i', 'o', 'u']
+
+def is_vowel2(x):
+    x = x.lower()
+    return x in 'aeiou' and len(x) == 1
+
+def is_vowel3(x):
+    vowels = 'aeiou'
+    for vowel in vowels:
+        if letter.lower() = vowel:
+            return True
+    return False
 
 #3
 def is_consonant(x):
@@ -20,6 +32,12 @@ def is_consonant(x):
         return False
     else:
         return True
+
+def is_consonant(x):
+    return x in 'bcdfghjklmnpqrstvwxyz' and len(x)  == 1
+
+def is_consonant(x):
+    return len(x) == 1 and x.isalpha() and not is_vowel(x)
 
 #4
 def capitalize_word(word):
@@ -79,14 +97,53 @@ def remove_vowels(words):
         else:
             continue
     return(words)
+
+def remove_vowels(string):
+    string_without_vowels = ''
+    for c in string:
+        if not is_vowel(c):
+            string_without_vowels += 
+    return string_without_vowels
+
+def remove_vowels(string):
+    return "".join([c for c in string if not is_vowel])
        
 #10
 def normalize_name(string):
     string = string.lower()
+    string = string.strip()
     for char in string:
-        if char.upper() == True:
-            stirng = string.replace(char,"-")
+        if char == " ":
+            string = string.replace(char,"_")  
     return string
 
+def remove_special_characters(string):
+    return ''.join([c for c in string if c.isalnum() or c == ' '])
+
+def normalize_name(string):
+    with_out_special_characters = remove_special_chars(string)
+    return with_out_special_chars.lower().strip().replace(" ", "")
+
 #11
-def cumsum
+def cumsum(numbers):
+    total = 0
+    cumulative_sums = []
+    for n in numbers:
+        total += n
+        cumulative_sums.append(total)
+    return cumulative_sums
+        
+def cumsum(numbers)
+    return [sum(numbers[:i + 1]) for i, _ in enumerate(numbers)]
+
+#Bonus 1
+
+def twelveto24(string):
+    if (string[-2]) == "A"
+        string = string - string[-1] - string[-2]
+
+
+
+#12       
+def col_index(col_name)
+
